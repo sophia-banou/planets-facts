dfb51068f7822904bb747bacfd595fc933b78b24
sophia-banou <sdi2000136@di.uoa.gr>
2024-12-24 10:04:15 +0200

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sophia-banou.github.io",
+  base: "/planets-facts/",
   prefetch: true,
   integrations: [react()],
 });
